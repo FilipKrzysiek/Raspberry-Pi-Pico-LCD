819d40e7819c29c3a6fe3b53d9d17faa6c9410ba
FilipCysEk <krzysiek.swal@gmail.com>
2021-02-13 20:08:34 +0100

    Add missing constructors. Add define to support std::string and char[] texts

--- src/LCDgeneric.cpp
+++ src/LCDgeneric.cpp
@@ -4,33 +4,14 @@
 
 #include "LCDgeneric.h"
 
-void LCDgeneric::writeLine(unsigned short lineNum, std::string text) {
-    cursorMoveTo(getFirstAddressOfLine(lineNum));
-
-    for (int i = 0; i < lineSize; ++i) {
-        if (text.size() > i)
-            writeChar(text[i]);
-        else writeChar(' ');
-    }
-}
-
-uint LCDgeneric::getFirstAddressOfLine(unsigned short lineNumber) {
-    if (amountLines > 1) {
-        if(lineNumber % 2 == 1) {
-            return 0x40 | ((lineNumber / (amountLines / 2)) * lineSize);
-        } else {
-            return ((lineNumber / (amountLines / 2)) * lineSize);
-        }
-    } else
-        return 0;
-}
-
 LCDgeneric::LCDgeneric(const uint &pinReadWrite, const uint &pinCommandData, const uint &pinEnable, const uint *pinData)
         : LCDbase(pinReadWrite, pinCommandData, pinEnable, pinData) {}
 
 LCDgeneric::LCDgeneric(const uint &pinCommandData, const uint &pinEnable, const uint *pinData) : LCDbase(pinCommandData,
                                                                                                          pinEnable,
                                                                                                          pinData) {}
+
+#if USE_4_BIT_DATA_BUS == 1
 
 LCDgeneric::LCDgeneric(const uint &pinReadWrite, const uint &pinComandData, const uint &pinEnable, const uint &pinData0,
                        const uint &pinData1, const uint &pinData2, const uint &pinData3) : LCDbase(pinReadWrite,
@@ -42,3 +23,60 @@ LCDgeneric::LCDgeneric(const uint &pinReadWrite, const uint &pinComandData, cons
 LCDgeneric::LCDgeneric(const uint &pinCommandData, const uint &pinEnable, const uint &pinData0, const uint &pinData1,
                        const uint &pinData2, const uint &pinData3) : LCDbase(pinCommandData, pinEnable, pinData0,
                                                                              pinData1, pinData2, pinData3) {}
+
+#else
+
+LCDgeneric::LCDgeneric(const uint &pinReadWrite, const uint &pinComandData, const uint &pinEnable, const uint &pinData0,
+                 const uint &pinData1, const uint &pinData2, const uint &pinData3, const uint &pinData4, const uint &pinData5,
+                 const uint &pinData6, const uint &pinData7) :
+                 LCDbase(pinReadWrite, pinComandData, pinEnable, pinData0,pinData1, pinData2, pinData3, pinData4,
+                         pinData5, pinData6, pinData7) {}
+
+LCDgeneric::LCDgeneric(const uint &pinCommandData, const uint &pinEnable, const uint &pinData0, const uint &pinData1,
+                 const uint &pinData2, const uint &pinData3, const uint &pinData4, const uint &pinData5,
+                 const uint &pinData6, const uint &pinData7) :
+        LCDbase(pinComandData, pinEnable, pinData0,pinData1, pinData2, pinData3, pinData4, pinData5, pinData6,
+                pinData7) {}
+#endif
+
+#if LCD_GENERIC_USE_STRING == 1
+
+void LCDgeneric::writeLine(unsigned short lineNum, std::string text) {
+    cursorMoveTo(getFirstAddressOfLine(lineNum));
+
+    for (int i = 0; i < lineSize; ++i) {
+        if (text.size() > i)
+            writeChar(text[i]);
+        else writeChar(' ');
+    }
+}
+#else
+
+void LCDgeneric::writeLine(unsigned short lineNum, char *text) {
+    cursorMoveTo(getFirstAddressOfLine(lineNum));
+
+    int i = 0;
+    for (; i < lineSize; ++i) {
+        if (text[i] != '\0') {
+            writeChar(text[i]);
+            break;
+        }
+    }
+
+    for (; i < lineSize; ++i) {
+        writeChar(' ');
+    }
+}
+
+#endif
+
+uint LCDgeneric::getFirstAddressOfLine(unsigned short lineNumber) {
+    if (amountLines > 1) {
+        if (lineNumber % 2 == 1) {
+            return 0x40 | ((lineNumber / (amountLines / 2)) * lineSize);
+        } else {
+            return ((lineNumber / (amountLines / 2)) * lineSize);
+        }
+    } else
+        return 0;
+}
--- src/LCDgeneric.h
+++ src/LCDgeneric.h
@@ -5,7 +5,14 @@
 #ifndef BUTTON_FIRST_LCDGENERIC_H
 #define BUTTON_FIRST_LCDGENERIC_H
 
+#ifndef LCD_GENERIC_USE_STRING
+#define LCD_GENERIC_USE_STRING 0
+#endif
+
+#if LCD_GENERIC_USE_STRING == 1
 #include <string>
+#endif
+
 #include "LCDbase.h"
 
 
@@ -15,13 +22,29 @@ public:
 
     LCDgeneric(const uint &pinReadWrite, const uint &pinCommandData, const uint &pinEnable, const uint *pinData);
 
+#if USE_4_BIT_DATA_BUS == 1
+
     LCDgeneric(const uint &pinReadWrite, const uint &pinComandData, const uint &pinEnable, const uint &pinData0,
                const uint &pinData1, const uint &pinData2, const uint &pinData3);
 
     LCDgeneric(const uint &pinCommandData, const uint &pinEnable, const uint &pinData0, const uint &pinData1,
                const uint &pinData2, const uint &pinData3);
 
+#else
+    LCDgeneric(const uint &pinReadWrite, const uint &pinComandData, const uint &pinEnable, const uint &pinData0,
+            const uint &pinData1, const uint &pinData2, const uint &pinData3, const uint &pinData4, const uint &pinData5,
+            const uint &pinData6, const uint &pinData7);
+
+    LCDgeneric(const uint &pinCommandData, const uint &pinEnable, const uint &pinData0, const uint &pinData1,
+            const uint &pinData2, const uint &pinData3, const uint &pinData4, const uint &pinData5, const uint &pinData6,
+            const uint &pinData7);
+#endif
+
+#if LCD_GENERIC_USE_STRING == 1
     void writeLine(unsigned short lineNum, std::string text);
+#else
+    void writeLine(unsigned short lineNum, char text[]);
+#endif
 
 private:
     uint getFirstAddressOfLine(unsigned short lineNumber);
